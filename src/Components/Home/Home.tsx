eb69068bb172944d42aef0c30d19ce58ba1836e9
Etka A <ebacikoz@gmail.com>
2023-11-30 17:50:41 -0500

    Adds Navbar component to Home page.

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,16 +2,23 @@ import { useState } from "react";
 import Sidebar from "./Sidebar";
 import { useParams } from "react-router-dom";
 import { SidebarIcon } from "../../UI/Icons";
+import Navbar from "./Navbar";
 
 const Home = () => {
   const { listId } = useParams();
   const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(true);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   return (
     <div className="flex h-full w-full">
+      <Navbar
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        listId={listId}
+      />
       {!isSideBarOpen && (
         <div
-          className="absolute left-5 top-6 cursor-pointer transition duration-300 hover:bg-slate-700"
+          className="absolute left-5 top-6 cursor-pointer transition duration-300 hover:bg-slate-700 max-lg:hidden"
           onClick={() => {
             setIsSideBarOpen(true);
           }}
