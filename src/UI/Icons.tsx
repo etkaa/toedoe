d1e60f51812c7d984e839b14655643c5a9c6a202
Etka A <ebacikoz@gmail.com>
2023-11-30 17:49:41 -0500

    Adds new icons for mobile menu.

--- src/UI/Icons.tsx
+++ src/UI/Icons.tsx
@@ -45,6 +45,63 @@ export const PlusIcon: React.FC<IconProps> = ({ styles }) => {
   );
 };
 
+export const HamburgerIcon: React.FC<IconProps> = ({ styles }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={styles ? styles : "h-6 w-6"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+};
+
+export const CubeIcon: React.FC<IconProps> = ({ styles }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={styles ? styles : "h-6 w-6"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+      />
+    </svg>
+  );
+};
+
+export const CloseIcon: React.FC<IconProps> = ({ styles }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={styles ? styles : "h-6 w-6"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
 export const SidebarIcon: React.FC<IconProps> = ({ styles }) => {
   return (
     <svg
